fce0f2d1e3cf98ac2a952bdeab3579c725863a2e
xwen1000 <xingwen1000@126.com>
2020-10-30 07:27:54 +0000

    编辑用户信息

--- app/Http/Controllers/Api/UsersController.php
+++ app/Http/Controllers/Api/UsersController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Http\Resources\UserResource;
 use App\Http\Requests\Api\UserRequest;
 use Illuminate\Auth\AuthenticationException;
+use App\Models\Image;
 
 class UsersController extends Controller
 {
@@ -43,5 +44,22 @@ class UsersController extends Controller
     public function me(Request $request)
     {
         return (new UserResource($request->user()))->showSensitiveFields();
+    }
+
+    public function update(UserRequest $request)
+    {
+        $user = $request->user();
+
+        $attributes = $request->only(['name', 'email', 'introduction']);
+
+        if ($request->avatar_image_id) {
+            $image = Image::find($request->avatar_image_id);
+
+            $attributes['avatar'] = $image->path;
+        }
+
+        $user->update($attributes);
+
+        return (new UserResource($user))->showSensitiveFields();
     }
 }
